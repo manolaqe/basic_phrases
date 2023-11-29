8185a13c22fe3ec3f553a39ea7fcfa0e201d7668
Mihai-Andrei Manolache <mihai-andrei.manolache@ubisoft.com>
2023-11-29 18:35:16 +0200

    6th commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final player = AudioPlayer();
+  final AudioPlayer player = AudioPlayer();
   PlayerState playerState = PlayerState.stopped;
 
   @override
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
       if (playerState == PlayerState.completed) {
         setState(() {
-          for (var i = 0; i < basicPhrases.length; i++) {
+          for (int i = 0; i < basicPhrases.length; i++) {
             basicPhrases[i].romanianIsToggled = false;
             basicPhrases[i].germanIsToggled = false;
           }
@@ -60,150 +60,199 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  List<PhraseModel> basicPhrases = [
+  List<PhraseModel> basicPhrases = <PhraseModel>[
     PhraseModel(
-        romanianText: 'salut',
-        germanText: 'salut (Germana)',
-        romanianIsToggled: false,
-        germanIsToggled: false,
-        romanianAudioPath: "01.mp3",
-        germanAudioPath: "02.mp3"),
+      romanianText: 'salut',
+      germanText: 'salut (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '01.mp3',
+      germanAudioPath: '02.mp3',
+    ),
     PhraseModel(
-        romanianText: 'mă numesc',
-        germanText: 'mă numesc (Germana)',
-        romanianIsToggled: false,
-        germanIsToggled: false,
-        romanianAudioPath: "03.mp3",
-        germanAudioPath: "04.mp3"),
+      romanianText: 'mă numesc',
+      germanText: 'mă numesc (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '03.mp3',
+      germanAudioPath: '04.mp3',
+    ),
     PhraseModel(
-        romanianText: 'cum ești',
-        germanText: 'cum ești (Germana)',
-        romanianIsToggled: false,
-        germanIsToggled: false,
-        romanianAudioPath: "05.mp3",
-        germanAudioPath: "06.mp3"),
+      romanianText: 'cum ești',
+      germanText: 'cum ești (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '05.mp3',
+      germanAudioPath: '06.mp3',
+    ),
     PhraseModel(
-        romanianText: 'ce faci',
-        germanText: 'ce faci (Germana)',
-        romanianIsToggled: false,
-        germanIsToggled: false,
-        romanianAudioPath: "07.mp3",
-        germanAudioPath: "08.mp3"),
+      romanianText: 'ce faci',
+      germanText: 'ce faci (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '07.mp3',
+      germanAudioPath: '08.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'bună ziua',
+      germanText: 'bună ziua (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '01.mp3',
+      germanAudioPath: '02.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'mulțumesc',
+      germanText: 'mulțumesc (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '03.mp3',
+      germanAudioPath: '04.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'la revedere',
+      germanText: 'la revedere (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '05.mp3',
+      germanAudioPath: '06.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'scuze',
+      germanText: 'scuze (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '07.mp3',
+      germanAudioPath: '08.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'unde este',
+      germanText: 'unde este (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '01.mp3',
+      germanAudioPath: '02.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'ceasul',
+      germanText: 'ceasul (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '03.mp3',
+      germanAudioPath: '04.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'îmi place',
+      germanText: 'îmi place (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '05.mp3',
+      germanAudioPath: '06.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'nu înțeleg',
+      germanText: 'nu înțeleg (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '07.mp3',
+      germanAudioPath: '08.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'vă rog',
+      germanText: 'vă rog (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '01.mp3',
+      germanAudioPath: '02.mp3',
+    ),
+    PhraseModel(
+      romanianText: 'îmi pare rău',
+      germanText: 'îmi pare rău (Germana)',
+      romanianIsToggled: false,
+      germanIsToggled: false,
+      romanianAudioPath: '03.mp3',
+      germanAudioPath: '04.mp3',
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(
-              child: Text(widget.title,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w200, fontSize: 30))),
+          title: Center(child: Text(widget.title, style: const TextStyle(fontWeight: FontWeight.w200, fontSize: 30))),
         ),
         body: Center(
           child: ListView.builder(
               itemCount: basicPhrases.length,
-              itemBuilder: (context, index) {
+              itemBuilder: (BuildContext context, int index) {
                 return Expanded(
-                    child: Column(children: [
+                    child: Column(children: <Widget>[
                   Row(
-                    children: [
+                    children: <Widget>[
                       Expanded(
                           child: AspectRatio(
                               aspectRatio: 1,
                               child: GestureDetector(
-                                  onTapDown: (details) {
+                                  onTapDown: (TapDownDetails details) {
                                     if (playerState != PlayerState.playing) {
                                       setState(() {
-                                        basicPhrases[index].romanianIsToggled =
-                                            !basicPhrases[index]
-                                                .romanianIsToggled;
+                                        basicPhrases[index].romanianIsToggled = !basicPhrases[index].romanianIsToggled;
                                       });
                                     }
                                   },
-                                  onTapUp: (details) {
+                                  onTapUp: (TapUpDetails details) {
                                     if (playerState != PlayerState.playing) {
-                                      player.play(AssetSource(
-                                          basicPhrases[index]
-                                              .romanianAudioPath));
+                                      player.play(AssetSource(basicPhrases[index].romanianAudioPath));
                                     }
                                   },
                                   child: Container(
                                     margin: const EdgeInsets.all(20),
                                     decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                            begin: Alignment.bottomLeft,
-                                            colors: [
-                                              Theme.of(context)
-                                                  .colorScheme
-                                                  .onPrimary,
-                                              Theme.of(context)
-                                                  .colorScheme
-                                                  .primary
-                                            ]),
-                                        borderRadius:
-                                            BorderRadius.circular(30)),
-                                    child: playerState == PlayerState.playing &&
-                                            basicPhrases[index]
-                                                .romanianIsToggled
-                                        ? const Center(
-                                            child: CircularProgressIndicator(
-                                                color: Colors.green))
+                                        gradient: LinearGradient(begin: Alignment.bottomLeft, colors: <Color>[
+                                          Theme.of(context).colorScheme.onPrimary,
+                                          Theme.of(context).colorScheme.primary
+                                        ]),
+                                        borderRadius: BorderRadius.circular(30)),
+                                    child: playerState == PlayerState.playing && basicPhrases[index].romanianIsToggled
+                                        ? const Center(child: CircularProgressIndicator(color: Colors.green))
                                         : Center(
                                             child: Text(
                                             basicPhrases[index].romanianText,
                                             textAlign: TextAlign.center,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                fontSize: 20),
+                                            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 20),
                                           )),
                                   )))),
                       Expanded(
                           child: AspectRatio(
                               aspectRatio: 1,
                               child: GestureDetector(
-                                  onTapDown: (details) {
+                                  onTapDown: (TapDownDetails details) {
                                     if (playerState != PlayerState.playing) {
                                       setState(() {
-                                        basicPhrases[index].germanIsToggled =
-                                            !basicPhrases[index]
-                                                .germanIsToggled;
+                                        basicPhrases[index].germanIsToggled = !basicPhrases[index].germanIsToggled;
                                       });
                                     }
                                   },
-                                  onTapUp: (details) {
+                                  onTapUp: (TapUpDetails details) {
                                     if (playerState != PlayerState.playing) {
-                                      player.play(AssetSource(
-                                          basicPhrases[index].germanAudioPath));
+                                      player.play(AssetSource(basicPhrases[index].germanAudioPath));
                                     }
                                   },
                                   child: Container(
                                     margin: const EdgeInsets.all(20),
                                     decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                            begin: Alignment.bottomLeft,
-                                            colors: [
-                                              Theme.of(context)
-                                                  .colorScheme
-                                                  .primary,
-                                              Theme.of(context)
-                                                  .colorScheme
-                                                  .onPrimary
-                                            ]),
-                                        borderRadius:
-                                            BorderRadius.circular(30)),
-                                    child: playerState == PlayerState.playing &&
-                                            basicPhrases[index].germanIsToggled
-                                        ? const Center(
-                                            child: CircularProgressIndicator(
-                                                color: Colors.green))
+                                        gradient: LinearGradient(begin: Alignment.bottomLeft, colors: <Color>[
+                                          Theme.of(context).colorScheme.primary,
+                                          Theme.of(context).colorScheme.onPrimary
+                                        ]),
+                                        borderRadius: BorderRadius.circular(30)),
+                                    child: playerState == PlayerState.playing && basicPhrases[index].germanIsToggled
+                                        ? const Center(child: CircularProgressIndicator(color: Colors.green))
                                         : Center(
                                             child: Text(
                                             textAlign: TextAlign.center,
                                             basicPhrases[index].germanText,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                fontSize: 20),
+                                            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 20),
                                           )),
                                   )))),
                     ],
@@ -215,13 +264,6 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class PhraseModel {
-  String romanianText;
-  String germanText;
-  bool romanianIsToggled = false;
-  bool germanIsToggled = false;
-  String germanAudioPath;
-  String romanianAudioPath;
-
   PhraseModel(
       {required this.romanianText,
       required this.germanText,
@@ -229,4 +271,11 @@ class PhraseModel {
       required this.germanIsToggled,
       required this.romanianAudioPath,
       required this.germanAudioPath});
+
+  String romanianText;
+  String germanText;
+  bool romanianIsToggled = false;
+  bool germanIsToggled = false;
+  String germanAudioPath;
+  String romanianAudioPath;
 }
